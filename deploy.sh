48e96c20a3463d77bd947bc775bebe8fd7b4b727
xiaodong <875697126@qq.com>
2016-12-12 11:46:18 +0800

    finish shell

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,57 @@
 #!/bin/bash
 
-#if error stop shell
+# stop shell if error
+
 set -e
 set -o pipefail
 
-if [$# -lt 1i ]; then
+if [$# -lt 1]; then
     echo "Usage:"
     echo "  sh deploy.sh [target]"
-    echo "valid targets:[staging|alpha]"
+    echo "target:   [staging | alpha ]"
     echo ""
-    echo "example: sh deploy.sh staging"
-exit 1
+    echo "etc:  sh deploy.sh staging"
+    echo ""
+    exit 1
 fi
 
-deploy_target=$1
+build_target=$1
 
 #init params
 port=4000
 path_git=${pwd}
 path_config=./game_dev.json
-path_deploy=/data/work/server/${deploy_target}
+path_deploy=/data/work/server/${build_target}
 
-echo "deploy start"
+#check params
+target_list=("staging" "alpha")
+if [[ " ${target_list[@]} " =~ " ${build_target} " ]] ; then
+    echo "build target : ${build_target}"
+else
+    echo "invalid target: ${build_target}"
+    exit 1
+fi
+
+echo "deploy begin"
 
 cd ${path_git}
 
+# replace config
+sed -i '' "s/send_mail_on_error\":false/send_mail_on_error\": true/g" ${path_config}
+#server port
+host_port=8000 
+#server ip
+host_ip=10.0.0.1
+#server user
+host_user_name=root
+# sync file to server
+rsync -avcrzl --delete-after ./game_dev.json  --exclude ./logs -e 'ssh -p ${host_port}' ${host_user_name}@${host_ip}:${path_deploy}
+
+git checkout ${path_config}
+
+# start server
+cmd="cd ${path_deploy} ; node pm2_start_server.js ${build_target};"
+ssh -tt -p ${host_port} ${host_user_name}@${host_ip} "${cmd}"
+
+echo "deploy complete!"
 
